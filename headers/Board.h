743e2e00540604748aa4fc1c666db4c4c476f884
lewis keech <lewis.keech@ada.ac.uk>
2021-03-26 20:09:06 +0000

    V3.1.1

--- headers/Board.h
+++ headers/Board.h
@@ -11,6 +11,6 @@ class Board{
   void renderOtherBoard();
   std::vector<std::string> destroyed(std::vector<std::vector<std::string>>);
   std::vector<std::vector<std::string>> boardState;
-  std::vector<std::string> toplineAlpha = {"a","b","c","d","e","f","g","h","i","j"}; 
+  std::vector<std::string> toplineAlpha = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","aa","ab","ac","ad","ae","af","ag","ah","ai","aj","ak","al","am","an","ao","ap","aq","ar","as","at","au","av","aw","ax","ay","az","b","b","b","b","b","b","b","b","b","b","b","b","b","b","","","","","","","","","","","","ca","cb"}; 
 };
 #endif
