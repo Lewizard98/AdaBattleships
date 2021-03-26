6596b8cd1ac4c37c961dbc12441c6451151b05b2
lewis keech <lewis.keech@ada.ac.uk>
2021-03-26 20:10:58 +0000

    V3.1.2

--- headers/Board.h
+++ headers/Board.h
@@ -11,6 +11,6 @@ class Board{
   void renderOtherBoard();
   std::vector<std::string> destroyed(std::vector<std::vector<std::string>>);
   std::vector<std::vector<std::string>> boardState;
-  std::vector<std::string> toplineAlpha = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","aa","ab","ac","ad","ae","af","ag","ah","ai","aj","ak","al","am","an","ao","ap","aq","ar","as","at","au","av","aw","ax","ay","az","b","b","b","b","b","b","b","b","b","b","b","b","b","b","","","","","","","","","","","","ca","cb"}; 
+  std::vector<std::string> toplineAlpha = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","aa","ab","ac","ad","ae","af","ag","ah","ai","aj","ak","al","am","an","ao","ap","aq","ar","as","at","au","av","aw","ax","ay","az","ba","bb","bc","bd","be","bf","bg","bh","bi","bj","bk","bl","bm","bn","bo","bp","bq","br","bs","bt","bu","bv","bw","bx","by","bz","ca","cb"}; 
 };
 #endif
